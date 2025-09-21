4f745ec24a36ddd571e34532c7b027de7b4db601
NotAlex575 <dapontealessandro89gmail.com>
2025-09-21 17:50:01 +0200

    creato scheletro estetico del risultato che deve venire nella milestone 4

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,90 @@
 const Homepage = () => {
   return (
-    <h1>HOMEPAGE</h1>
+    <div className="container my-5">
+            <div className="row">
+                <div className="col-12 text-center">
+                    <h1>Filmly</h1>
+                    <h2>
+                        <i>Film per i veri appassionati</i>
+                    </h2>
+                </div>
+            </div>
+            <div className="row gy-3">
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <img 
+                            src = "./img/sample.jpg" 
+                            className="img-fluid"
+                            alt = "film"
+                        /> 
+                        <div className="overlay">
+                            <h2 className="text-center my-3">titolo</h2>
+                            <p className="text-center">autore</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row gy-3">
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <img 
+                            src = "./img/sample.jpg" 
+                            className="img-fluid"
+                            alt = "film"
+                        /> 
+                        <div className="overlay">
+                            <h2 className="text-center my-3">titolo</h2>
+                            <p className="text-center">autore</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row gy-3">
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <img 
+                            src = "./img/sample.jpg" 
+                            className="img-fluid"
+                            alt = "film"
+                        /> 
+                        <div className="overlay">
+                            <h2 className="text-center my-3">titolo</h2>
+                            <p className="text-center">autore</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row gy-3">
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <img 
+                            src = "./img/sample.jpg" 
+                            className="img-fluid"
+                            alt = "film"
+                        /> 
+                        <div className="overlay">
+                            <h2 className="text-center my-3">titolo</h2>
+                            <p className="text-center">autore</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row gy-3">
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <img 
+                            src = "./img/sample.jpg" 
+                            className="img-fluid"
+                            alt = "film"
+                        /> 
+                        <div className="overlay">
+                            <h2 className="text-center my-3">titolo</h2>
+                            <p className="text-center">autore</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
   )
 }
 
